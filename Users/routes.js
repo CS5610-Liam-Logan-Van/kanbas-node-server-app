eb4b0ff7e74fa08e1ac37b8c737e2fe30b908122
Logan Willans <156117377+loganjwillans@users.noreply.github.com>
2024-08-13 10:24:59 -0400

    a6

--- Users/routes.js
+++ Users/routes.js
@@ -1,7 +1,6 @@
 import * as dao from "./dao.js";
 
-let currentUser = null;
-export default function UserRoutes(app) {
+function UserRoutes(app) {
     const createUser = async (req, res) => {
         const user = await dao.createUser(req.body);
         res.json(user);
@@ -27,7 +26,6 @@ export default function UserRoutes(app) {
 
         const users = await dao.findAllUsers();
         res.json(users);
-        return;
     };
 
 
@@ -45,20 +43,52 @@ export default function UserRoutes(app) {
 
     };
     const signup = async (req, res) => {
+        const user = await dao.findUserByUsername(req.body.username);
+        if (user) {
+            res.status(400).json(
+                {message: "Username already taken"});
+            return;
+        }
+        const currentUser = await dao.createUser(req.body);
+        req.session["currentUser"] = currentUser;
+        res.json(currentUser);
     };
+
     const signin = async (req, res) => {
+        const {username, password} = req.body;
+        const currentUser = await dao.findUserByCredentials(username, password);
+        if (currentUser) {
+            req.session["currentUser"] = currentUser;
+            res.json(currentUser);
+        } else {
+            res.status(401).json({message: "Unable to login. Try again later."});
+        }
     };
+
+    const profile = (req, res) => {
+        const currentUser = req.session["currentUser"];
+        if (!currentUser) {
+            res.sendStatus(401);
+            return;
+        }
+        res.json(currentUser);
+    };
+
     const signout = (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200);
     };
-    const profile = async (req, res) => {
-    };
+
+
     app.post("/api/users", createUser);
     app.get("/api/users", findAllUsers);
     app.get("/api/users/:userId", findUserById);
     app.put("/api/users/:userId", updateUser);
     app.delete("/api/users/:userId", deleteUser);
     app.post("/api/users/signup", signup);
-    app.post("/api/users/signin", signin);
     app.post("/api/users/signout", signout);
+    app.post("/api/users/signin", signin);
     app.post("/api/users/profile", profile);
 }
+
+export default UserRoutes;
